4496b562543f2869678712f0042da23f28fab39b
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-02-06 15:23:51 +0700

    Calculating a Total Score, Getters & "else-if"

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -4,7 +4,7 @@ import './answer.dart';
 
 class Quiz extends StatelessWidget {
   final List<Map<String, Object>> questions;
-  final VoidCallback answerQuestion;
+  final Function answerQuestion;
   final int questionIndex;
 
   Quiz(
@@ -15,10 +15,14 @@ class Quiz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[
-        Question(questions[questionIndex]['questionText'] as String),
-        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
-          return Answer(answerQuestion, answer);
+      children: [
+        Question(
+          questions[questionIndex]['questionText'] as String,
+        ),
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(
+              () => answerQuestion(answer['score']), answer['text'] as String);
         }).toList()
       ],
     );
